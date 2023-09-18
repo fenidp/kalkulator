c2c3ed7657ef438670f7c637071a0c3ce63d3ed2
fenidp <114385571+fenidp@users.noreply.github.com>
2023-09-18 10:25:38 +0700

    Initial commit

---
+++ app/src/main/java/com/example/kalkulator/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.kalkulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    //deklarasi
+    private lateinit var panjang : EditText
+    private lateinit var lebar: EditText
+    private lateinit var tinggi : EditText
+    private lateinit var btn: Button
+    private lateinit var hasil: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //inisialisai variabel
+        panjang = findViewById(R.id.edt_length)
+        lebar = findViewById(R.id.edt_width)
+        tinggi = findViewById(R.id.edt_height)
+        btn = findViewById(R.id.btn_result)
+        hasil = findViewById(R.id.result)
+
+        btn.setOnClickListener(this)
+    }
+
+    override fun onClick(v : View?) {
+        if (v?.id==R.id.btn_result){
+            val input_panjang = panjang.text.toString().trim()
+            val input_lebar = lebar.text.toString().trim()
+            val input_tinggi = tinggi.text.toString().trim()
+            val volume = input_panjang.toDouble()*input_lebar.toDouble()*input_tinggi.toDouble()
+            hasil.text = volume.toString()
+        }
+    }
+}
